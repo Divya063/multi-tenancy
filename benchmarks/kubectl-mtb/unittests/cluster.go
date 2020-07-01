2e841f76e58d9f1fcf8b163a9ef30ede7bbb7716
Divya Rani <ranidivya063@gmail.com>
2020-07-01 19:17:46 +0530

    added controllers

--- benchmarks/kubectl-mtb/unittests/cluster.go
+++ benchmarks/kubectl-mtb/unittests/cluster.go
@@ -3,12 +3,32 @@ package unittests
 import (
 	"fmt"
 	"log"
+	"sync"
+
+	"github.com/onsi/gomega"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
 	"sigs.k8s.io/kind/pkg/apis/config/v1alpha4"
 
 	"k8s.io/client-go/kubernetes"
-"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/tools/clientcmd"
 	"sigs.k8s.io/kind/pkg/cluster"
 )
+
+func getUniqueName(str, a string) string {
+	return fmt.Sprintf("%+v-%+v", str, a)
+}
+
+// StartTestManager adds recFn
+func StartTestManager(mgr manager.Manager, g *gomega.GomegaWithT) (chan struct{}, *sync.WaitGroup) {
+	stop := make(chan struct{})
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		g.Expect(mgr.Start(stop)).NotTo(gomega.HaveOccurred())
+	}()
+	return stop, wg
+}
 
 func CreateCluster() (*cluster.Provider, string, string, error) {
 	clusterName := "kubectl-mtb"
@@ -36,12 +56,13 @@ func CreateCluster() (*cluster.Provider, string, string, error) {
 	//fmt.Printf("ctx.KubeConfigPath(): %s\n", newProvider.List)
 	kubeConfig, err := newProvider.KubeConfig(clusterName, false)
 
-	if err!=nil {
+	if err != nil {
 		log.Println(err.Error())
 	}
 
+	fmt.Println("cluster is created")
 
-	return newProvider, kubeConfig, clusterName,  nil
+	return newProvider, kubeConfig, clusterName, nil
 }
 
 func CreateTenants() {
@@ -61,7 +82,7 @@ func getClientSet(configPath string) (*kubernetes.Clientset, error) {
 	return clientset, nil
 }
 
-func DeleteCluster(provider *cluster.Provider, name string, configPath string) (error) {
+func DeleteCluster(provider *cluster.Provider, name string, configPath string) error {
 
 	err := provider.Delete(name, configPath)
 	fmt.Println(err)
@@ -73,4 +94,17 @@ func DeleteCluster(provider *cluster.Provider, name string, configPath string) (
 
 }
 
+// func main() {
 
+// 	provider, config, name, err := CreateCluster()
+// 	if err != nil {
+// 		fmt.Println(err)
+// 	}
+
+// 	// fmt.Println(provider, config, name)
+
+// 	err = DeleteCluster(provider, name, config)
+// 	if err != nil {
+// 		fmt.Println(err)
+// 	}
+// }
--- benchmarks/kubectl-mtb/unittests/utitlity_test.go
+++ benchmarks/kubectl-mtb/unittests/utitlity_test.go
@@ -5,26 +5,34 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
-	"sigs.k8s.io/kind/pkg/cluster"
 	"testing"
+	"time"
 
+	corev1 "k8s.io/api/core/v1"
+	v1 "k8s.io/api/rbac/v1"
+	"sigs.k8s.io/kind/pkg/cluster"
+
+	"github.com/kubernetes-sigs/multi-tenancy/tenant/pkg/apis"
 	tenancyv1alpha1 "github.com/kubernetes-sigs/multi-tenancy/tenant/pkg/apis/tenancy/v1alpha1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	tenant2 "github.com/kubernetes-sigs/multi-tenancy/tenant/pkg/controller/tenant"
+	tenantnamespace "github.com/kubernetes-sigs/multi-tenancy/tenant/pkg/controller/tenantnamespace"
+	"github.com/onsi/gomega"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/rest"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/envtest"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
-	"k8s.io/client-go/kubernetes/scheme"
-	"github.com/kubernetes-sigs/multi-tenancy/tenant/pkg/apis"
 )
 
 var cfg *rest.Config
 var c client.Client
 var err error
 
+const timeout = time.Second * 5
+
 func TestMain(m *testing.M) {
-	provider, kubeConfig, clusterName, err := setup()
+	// provider, kubeConfig, clusterName, err := setup()
 	tr := true
 	apis.AddToScheme(scheme.Scheme)
 	fmt.Println()
@@ -39,28 +47,46 @@ func TestMain(m *testing.M) {
 	//fmt.Println(cfg)
 	code := m.Run()
 	t.Stop()
-	teardown(provider, kubeConfig, clusterName)
+	// teardown(provider, kubeConfig, clusterName)
 	os.Exit(code)
 }
 
 func TestCreateTenant(t *testing.T) {
 
+	g := gomega.NewGomegaWithT(t)
+
+	sa := corev1.ServiceAccount{
+		TypeMeta: metav1.TypeMeta{
+			Kind: "ServiceAccount",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "tenant-admin-sb",
+			Namespace: "default",
+		},
+	}
 	tenant := &tenancyv1alpha1.Tenant{
 		ObjectMeta: metav1.ObjectMeta{
-			Name: "check",
+			Name: "tenant-b",
 		},
 		Spec: tenancyv1alpha1.TenantSpec{
-			TenantAdminNamespaceName: "t1admin",
+			TenantAdminNamespaceName: "tenant-admin-sa",
+			TenantAdmins: []v1.Subject{
+				{
+					Kind:      "ServiceAccount",
+					Name:      sa.ObjectMeta.Name,
+					Namespace: sa.ObjectMeta.Namespace,
+				},
+			},
 		},
 	}
 
 	instance := &tenancyv1alpha1.TenantNamespace{
 		ObjectMeta: metav1.ObjectMeta{
-			Name:   "t1admin",
+			Name:      "tenant1ns",
 			Namespace: tenant.Spec.TenantAdminNamespaceName,
 		},
 		Spec: tenancyv1alpha1.TenantNamespaceSpec{
-			Name: "t1admi",
+			Name: "t1admin",
 		},
 	}
 
@@ -68,18 +94,34 @@ func TestCreateTenant(t *testing.T) {
 	if err != nil {
 		fmt.Println(err)
 	}
+
 	c = mgr.GetClient()
-	err = tenant2.Add(mgr)
+
+	//start tenant controller
+	recFnTenant, _ := tenant2.SetupTestReconcile(tenant2.SetupNewReconciler(mgr))
+	g.Expect(tenant2.AddManager(mgr, recFnTenant)).NotTo(gomega.HaveOccurred())
+
+	//start tenantnamespace control
+	err = tenantnamespace.Add(mgr)
 	if err != nil {
 		fmt.Println(err)
 	}
 
+	stopMgr, mgrStopped := StartTestManager(mgr, g)
+	defer func() {
+		close(stopMgr)
+		mgrStopped.Wait()
+	}()
+	err = c.Create(context.TODO(), &sa)
+	if err != nil {
+		t.Logf("Failed to create tenant admin: %+v with error: %+v", sa.ObjectMeta.Name, err)
+		return
+	}
 
 	err = c.Create(context.TODO(), tenant)
 	if err != nil {
 		fmt.Println(err)
 	}
-
 	err = c.Create(context.TODO(), instance)
 	if err != nil {
 		fmt.Println(err)
@@ -87,11 +129,10 @@ func TestCreateTenant(t *testing.T) {
 
 }
 
-func setup() (*cluster.Provider, string, string, error){
+func setup() (*cluster.Provider, string, string, error) {
 	// Do something here.
 
 	provider, kubeConfig, clusterName, err := CreateCluster()
-
 
 	if err != nil {
 		fmt.Printf("\033[1;36m%s\033[0m", "> Setup completed\n")
@@ -104,7 +145,6 @@ func setup() (*cluster.Provider, string, string, error){
 }
 
 func teardown(provider *cluster.Provider, kubeConfig string, cluster string) {
-
 
 	DeleteCluster(provider, cluster, kubeConfig)
 	fmt.Printf("\033[1;36m%s\033[0m", "> Teardown completed")
